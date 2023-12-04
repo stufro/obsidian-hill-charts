ee422f7f99b260fc5fd387587bb3615621eb07ea
Stuart Frost <stuart.2.frost@bt.com>
2023-12-04 16:41:28 +0000

    Add basic hard coded hill chart

--- main.ts
+++ main.ts
@@ -1,85 +1,86 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { create, scaleLinear, select, axisBottom, range, line } from 'd3';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface HillChartSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: HillChartSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HillCharts extends Plugin {
+	settings: HillChartSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerMarkdownCodeBlockProcessor("hillchart", (source, el, ctx) => {
+			const chartHeight = 250;
+			const chartWidth = 700;
+			const container = create("svg");
+			container
+				.attr("width", "700")
+				.attr("height", "260")
+				.attr("id", "hillChart");
+			el.parentElement?.replaceChild(container.node(), el);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			const marginTop = 5;
+			const xScale = scaleLinear().domain([0, 100]).range([0, chartWidth]);
+			const yScale = scaleLinear().domain([0, 110]).range([chartHeight, 0]);
+
+			const hillFn = (point : number) =>
+			  50 * Math.sin((Math.PI / 50) * point - (1 / 2) * Math.PI) + 50;
+
+			// Render bottom line
+			const bottomLine = axisBottom(xScale).ticks(0).tickSize(0);
+			container
+				.append('g')
+				.attr('class', 'hill-chart-bottom-line')
+				.attr('transform', `translate(0, ${chartHeight + marginTop})`)
+				.call(bottomLine);
+
+			// Render main curve
+			const mainLineCurvePoints = range(0, 100, 0.1).map((i) => ({
+			  x: i,
+			  y: 50 * Math.sin((Math.PI / 50) * i - (1 / 2) * Math.PI) + 50,
+			}));
+
+			const curve = line()
+			  .x((d) => xScale(d.x))
+			  .y((d) => yScale(d.y));
+
+			container
+			  .append('path')
+			  .attr('class', 'chart-hill-main-curve')
+			  .datum(mainLineCurvePoints)
+			  .attr('d', curve)
+			  .style('fill', "transparent")
+			  .style('stroke', "white");
+
+			// Render middle dotted line
+			container
+			  .append('line')
+			  .attr('class', 'hill-chart-middle-line')
+			  .attr('y1', yScale(0))
+			  .attr('y2', yScale(100))
+			  .attr('x2', xScale(50))
+			  .attr('x1', xScale(50))
+			  .style('stroke', "lightgrey")
+			  .style('stroke-dasharray', "5,5");
+
+			// Render circle
+			const percentage = 40;
+			container.append("circle")
+				.attr("cx", xScale(percentage))
+				.attr("cy", yScale(hillFn(percentage)))
+				.attr("r", 10)
+				.style("fill", "red");
+			});
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -91,26 +92,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HillCharts;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HillCharts) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
