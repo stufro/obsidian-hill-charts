57954ddb64907ffbafe9b6fcdc11d4bfbe6c7536
Stuart Frost <stuart.2.frost@bt.com>
2023-12-05 13:03:02 +0000

    Remove fixture file

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -3,23 +3,16 @@ import { readFile } from 'fs/promises';
 
 describe('parseCodeBlock', () => {
   test('parses the code block into an object', async () => {
-    await readFixture(`${__dirname}/fixtures/code_block.yml`)
-      .then((fixture) => {
-        expect(parseCodeBlock(fixture)).toEqual([
-          { percentage: 50, color: "red" },
-          { percentage: 20, color: "blue" },
-        ])
-      })
+    const yaml = `points:
+                    - percentage: 50
+                      color: red
+                      text: Foo
+                    - percentage: 20
+                      color: blue`
+
+    expect(parseCodeBlock(yaml)).toEqual([
+      { percentage: 50, color: "red", text: "Foo" },
+      { percentage: 20, color: "blue" },
+    ])
   });
 });
-
-async function readFixture(path: string) {
-  let data = "";
-  try {
-    data = await readFile(path, 'utf8');
-  } catch (err) {
-    console.error(err);
-  }
-
-  return data
-}
