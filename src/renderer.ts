487a2f21ac2884a1ed773706923cf55772428570
Stuart Frost <stuart.2.frost@bt.com>
2023-12-06 17:43:38 +0000

    Return node elements from render functions
    Rather than passing container around and appending to it.

--- src/renderer.ts
+++ src/renderer.ts
@@ -19,12 +19,14 @@ function renderHillChart(data: Array<ChartPoint>) {
   const xScale = scaleLinear().domain([0, 100]).range([0, chartWidth]);
   const yScale = scaleLinear().domain([0, 110]).range([chartHeight, 0]);
 
-  renderBaseLine(container, xScale, chartHeight, marginTop);
-  renderCurve(container, xScale, yScale);
-  renderMiddleLine(container, xScale, yScale);
-  renderFooterText(container, xScale, chartHeight);
+  container.append(() => renderBaseLine(xScale, chartHeight, marginTop));
+  container.append(() => renderCurve(xScale, yScale));
+  container.append(() => renderMiddleLine(xScale, yScale));
+  container.append(() => renderLeftFooterText(xScale, chartHeight));
+  container.append(() => renderRightFooterText(xScale, chartHeight));
   data?.forEach(point => {
-    renderPoint(container, xScale, yScale, point);
+    container.append(() => renderPoint(xScale, yScale, point));
+    container.append(() => renderPointLabel(xScale, yScale, point));
   })
 
   return container;
@@ -34,16 +36,17 @@ function hillFn(point: number) {
   return 50 * Math.sin((Math.PI / 50) * point - (1 / 2) * Math.PI) + 50;
 }
 
-function renderBaseLine(container: any, xScale: any, chartHeight: number, marginTop: number) {
+function renderBaseLine(xScale: any, chartHeight: number, marginTop: number) {
   const bottomLine = axisBottom(xScale).ticks(0).tickSize(0);
-  container
-    .append('g')
+
+  return create<SVGGElement>('svg:g')
     .attr('class', 'hill-chart-bottom-line')
     .attr('transform', `translate(0, ${chartHeight + marginTop})`)
-    .call(bottomLine);
+    .call(bottomLine)
+    .node();
 }
 
-function renderCurve(container: any, xScale: any, yScale: any) {
+function renderCurve(xScale: any, yScale: any) {
   const mainLineCurvePoints = range(0, 100, 0.1).map((i) => ({
     x: i,
     y: hillFn(i),
@@ -51,42 +54,46 @@ function renderCurve(container: any, xScale: any, yScale: any) {
 
   const curve = line()
     .x((d: any) => xScale(d.x))
-    .y((d: any) => yScale(d.y));
+    .y((d: any) => yScale(d.y)) as any;
 
-  container
-    .append('path')
+  return create('svg:path')
     .attr('class', 'chart-hill-main-curve')
     .datum(mainLineCurvePoints)
     .attr('d', curve)
     .style('fill', "transparent")
-    .style('stroke', "white");
+    .style('stroke', "white")
+    .node();
 }
 
-function renderMiddleLine(container: any, xScale: any, yScale: any) {
-  container
-    .append('line')
+function renderMiddleLine(xScale: any, yScale: any) {
+  return create('svg:line')
     .attr('class', 'hill-chart-middle-line')
     .attr('y1', yScale(0))
     .attr('y2', yScale(100))
     .attr('x2', xScale(50))
     .attr('x1', xScale(50))
     .style('stroke', "lightgrey")
-    .style('stroke-dasharray', "5,5");
+    .style('stroke-dasharray', "5,5")
+    .node();
 }
 
-function renderPoint(container: any, xScale: any, yScale: any, point: ChartPoint) {
-  container.append("circle")
+function renderPoint(xScale: any, yScale: any, point: ChartPoint) {
+  return create("svg:circle")
     .attr("cx", xScale(point.position))
     .attr("cy", yScale(hillFn(point.position)))
     .attr("r", 10)
     .style("fill", point.color)
-    .style("opacity", "0.7");
+    .style("opacity", "0.7")
+    .node();
+}
 
-  container.append("text")
-    .text(point.text)
+function renderPointLabel(xScale: any, yScale: any, point: ChartPoint) {
+  return create("svg:text")
+    .text(point.text || "")
     .attr("x", adjustedXPosition(xScale(point.position), point.position))
     .attr("y", yScale(hillFn(point.position)) + 5)
     .style("fill", "lightgrey")
+    .node();
 }
 
 function adjustedXPosition(coordinate: number, position: number) {
@@ -95,23 +102,27 @@ function adjustedXPosition(coordinate: number, position: number) {
   return coordinate + adjustment
 }
 
-function renderFooterText(container: any, xScale: any, chartHeight: number) {
-  container
-    .append('text')
+function renderLeftFooterText(xScale: any, chartHeight: number) {
+  return create('svg:text')
     .attr('class', 'hill-chart-text')
     .text('Figuring things out')
     .style('font-size', "1rem")
     .style('fill', "lightgrey")
     .attr('x', xScale(20))
-    .attr('y', chartHeight + 30);
-  container
+    .attr('y', chartHeight + 30)
+    .node();
+}
+
+function renderRightFooterText(xScale: any, chartHeight: number) {
+  return create('svg:text')
     .append('text')
     .attr('class', 'hill-chart-text')
     .text('Making it happen')
     .style('font-size', "1rem")
     .style('fill', "lightgrey")
     .attr('x', xScale(60))
-    .attr('y', chartHeight + 30);
+    .attr('y', chartHeight + 30)
+    .node();
 }
 
 export { renderHillChart, adjustedXPosition };
