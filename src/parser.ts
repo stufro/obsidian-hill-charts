f41b61eb13d13bc7062feb84e823968f63ccce9d
Stuart Frost <stuart.2.frost@bt.com>
2023-12-10 15:42:48 +0000

    Refactor color selection

--- src/parser.ts
+++ src/parser.ts
@@ -1,20 +1,21 @@
 import { parse } from 'yaml'
 import { HillChartSettings, ChartPoint } from './types'
 
+const DEFAULT_COLORS = [
+  "var(--color-red)",
+  "var(--color-blue)",
+  "var(--color-orange)",
+  "var(--color-cyan)",
+  "var(--color-green)",
+  "var(--color-yellow)",
+  "var(--color-pink)",
+]
+
 function parseCodeBlock(input: string, settings: HillChartSettings): Array<ChartPoint> {
-  const defaultColors = [
-    "var(--color-red)",
-    "var(--color-blue)",
-    "var(--color-orange)",
-    "var(--color-cyan)",
-    "var(--color-green)",
-    "var(--color-yellow)",
-    "var(--color-pink)",
-  ]
-  let colors = [...defaultColors];
+  let colors = [...DEFAULT_COLORS];
 
   return parse(input)["points"].map((point: ChartPoint) => {
-    if (colors.length == 0) colors = [...defaultColors];
+    if (colors.length == 0) colors = [...DEFAULT_COLORS];
     return { size: settings.pointSize, color: selectColor(colors), ...point }
   })
 }
