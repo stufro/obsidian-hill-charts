f023cf79bac906f0ebc7868c709f1d761df69615
Stuart Frost <stuart.2.frost@bt.com>
2024-03-04 12:16:58 +0000

    Replace tabs with spaces in main.ts

--- main.ts
+++ main.ts
@@ -7,39 +7,39 @@ import { SettingTab, DEFAULT_SETTINGS } from 'src/settings';
 const DEFAULT_ERROR = "An error occured rendering this hill chart. Please inspect the console logs."
 
 export default class HillCharts extends Plugin {
-	settings: HillChartSettings;
+  settings: HillChartSettings;
 
-	async onload() {
-		await this.loadSettings();
-		this.addSettingTab(new SettingTab(this.app, this));
+  async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new SettingTab(this.app, this));
 
-		this.registerMarkdownCodeBlockProcessor("hillchart", (source, el, ctx) => {
-			const parsedInput = parseCodeBlock(source, this.settings, parseYaml)
+    this.registerMarkdownCodeBlockProcessor("hillchart", (source, el, _ctx) => {
+      const parsedInput = parseCodeBlock(source, this.settings, parseYaml)
 
-			if (parsedInput.ok) {
-				const container = renderHillChart(parsedInput.points || [], this.settings);
-				el.parentElement?.replaceChild(container || this.errorMessage(), el);
-			} else {
-				new Notice(parsedInput.error || DEFAULT_ERROR)
-				el.parentElement?.replaceChild(this.errorMessage(parsedInput.error), el);
-			}
-		});
-	}
+      if (parsedInput.ok) {
+        const container = renderHillChart(parsedInput.points || [], this.settings);
+        el.parentElement?.replaceChild(container || this.errorMessage(), el);
+      } else {
+        new Notice(parsedInput.error || DEFAULT_ERROR)
+        el.parentElement?.replaceChild(this.errorMessage(parsedInput.error), el);
+      }
+    });
+  }
 
-	onunload() {
-	}
+  onunload() {
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 
-	errorMessage(error = ""): Node {
-		const element = document.createElement("i")
-		element.textContent = `${DEFAULT_ERROR} ${error}`;
-		return element;
-	}
+  errorMessage(error = ""): Node {
+    const element = document.createElement("i")
+    element.textContent = `${DEFAULT_ERROR} ${error}`;
+    return element;
+  }
 }
